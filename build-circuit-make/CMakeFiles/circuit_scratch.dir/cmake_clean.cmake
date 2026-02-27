c27eda6acec856ef398be23d8ac47c432d6e1762
Arya Kakodkar <kakodkararya@gmail.com>
2026-02-27 21:39:05 +0000

    feat: added architecture for erasure errors to be encoded in Stim circuit

---
+++ build-circuit-make/CMakeFiles/circuit_scratch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circuit_scratch.dir/Users/aryakakodkar/Documents/qerasure/apps/circuit_scratch.cc.o"
+  "CMakeFiles/circuit_scratch.dir/Users/aryakakodkar/Documents/qerasure/apps/circuit_scratch.cc.o.d"
+  "CMakeFiles/circuit_scratch.dir/Users/aryakakodkar/Documents/qerasure/src/core/circuit/circuit.cc.o"
+  "CMakeFiles/circuit_scratch.dir/Users/aryakakodkar/Documents/qerasure/src/core/circuit/circuit.cc.o.d"
+  "circuit_scratch"
+  "circuit_scratch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circuit_scratch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
